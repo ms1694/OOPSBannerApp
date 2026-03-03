e42897f2259452195cf9c79accb84c30158287f9
ms1694 <ms1694@srmist.edu.in>
2026-03-03 11:03:36 +0530

    added UC6

--- oopsBannerApp.java
+++ oopsBannerApp.java
@@ -74,7 +74,7 @@
         }
     }
 }*/
-public class oopsBannerApp {
+/*public class oopsBannerApp {
     public static void main(String[] args) {
         char[][] O = {
             {' ', '*', '*', '*', ' '},
@@ -112,4 +112,47 @@ public class oopsBannerApp {
             System.out.println();
         }
     }
+}*/
+public class oopsBannerApp{
+
+ public static String[] getOPattern(){
+	 return new String[] {
+		 "     ***     ",
+         " **     **   ", 
+         "**        ** ", 
+         "**        ** ", 
+         "**        ** ", 
+         " **      **  ", 
+	     "   ***       " };
+ }
+ public static String[] getPPattern() {
+	 return new String[] {
+		  " *******    " ,
+          " *      *   " , 
+          " *      *   " ,
+          " *******    " , 
+          " *          " , 
+	      " *          " , 
+		  " *          "};
+ }
+ public static String[] getSPattern() {
+	 return new String[] {
+		  "   *****   " , 
+          " *         " ,
+          " *         "  ,
+          "   *****   "  ,
+          "         * "  ,
+          "         * "  ,
+          "  ******   "  ,
+            };
+ }
+ public static void main(String[] args) {
+	 String[] oPattern = getOPattern();
+	 String[] pPattern = getPPattern();
+     String[] SPattern = getSPattern(); 
+	 
+ for (int i = 0; i < oPattern.length; i++) {
+	 System.out.println (oPattern[i] + " " +oPattern[i]+" "+pPattern[i]+" "+SPattern[i]);
+        }
+     }
 }
