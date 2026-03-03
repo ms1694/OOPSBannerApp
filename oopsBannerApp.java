ff2c4ccf09393a4a7a3bb57fc7a18999b21a3f84
ms1694 <ms1694@srmist.edu.in>
2026-03-03 09:04:19 +0530

    added UC4

--- oopsBannerApp.java
+++ oopsBannerApp.java
@@ -17,4 +17,40 @@
 
     }
 }*/
+/*public class OOPSBannerApp3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("", "      ***      ***      *****       ***** "));
+        System.out.println(String.join("", "     ** **    ** **    **   **    **      "));
+        System.out.println(String.join("", "    **   **  **   **   **   **   **       "));
+        System.out.println(String.join("", "    **   **  **   **   *****      ***     "));
+        System.out.println(String.join("", "    **   **  **   **   **            **   "));
+        System.out.println(String.join("", "     ** **    ** **    **             **  "));
+        System.out.println(String.join("", "      ***      ***     **        *****    "));
+
+    }
+}*/
+public class oopsBannerApp {
+
+    public static void main(String[] args) {
+
+    
+        String[] banner = new String[7];
+
+    
+        banner[0] = String.join("", "      ***      ***      *****       ***** ");
+        banner[1] = String.join("", "     ** **    ** **    **   **    **      ");
+        banner[2] = String.join("", "    **   **  **   **   **   **   **       ");
+        banner[3] = String.join("", "    **   **  **   **   *****      ***     ");
+        banner[4] = String.join("", "    **   **  **   **   **            **   ");
+        banner[5] = String.join("", "     ** **    ** **    **             **  ");
+        banner[6] = String.join("", "      ***      ***     **        *****    ");
+
+        
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
+
 
