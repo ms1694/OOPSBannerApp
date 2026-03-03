a74861ca33251dc54622d94e395d3007e1add7ed
ms1694 <ms1694@srmist.edu.in>
2026-03-03 08:54:45 +0530

    added UC2

--- oopsBannerApp.java
+++ oopsBannerApp.java
@@ -4,7 +4,7 @@
     }
 }*/
 
-public class oopsBannerApp {
+/*public class oopsBannerApp {
     public static void main(String[] args) {
 
         System.out.println("      ***      ***      *****       *****      ");
@@ -16,4 +16,5 @@ public class oopsBannerApp {
         System.out.println("      ***      ***     **        *****         ");
 
     }
-}
+}*/
+
